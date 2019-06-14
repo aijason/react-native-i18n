26a200b48ff09d1ff67f19e0aa6239d63991c0a4
Jean-Frédéric Durand <jfdurk@gmx.fr>
2019-06-14 13:49:29 -0400

    add custom mock to handle test case

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const I18nJs = require('i18n-js');
 if (typeof RNI18n !== 'undefined') {
   I18nJs.locale = RNI18n.languages[0];
 } else if (__DEV__) {
-  console.warn('react-native-i18n module is not correctly linked');
+  // /!\ react-native-i18n is not linked
+  I18nJs.locale = 'en'; // a locale from your available translations
+  I18nJs.displayDate = (format, date) => I18nJs.l(`date.formats.${format}`, date);
 }
 
 export const getLanguages = () => RNI18n.getLanguages();
